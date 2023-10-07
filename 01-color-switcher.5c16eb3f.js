b7648e959a4d38a10e887786d80f91a0e3b55daf
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-10-07 07:44:28 +0000

    Deploying to gh-pages from @ dmytro26pyvovar/JSPyvovar09@4178cb6a1cca7ad99ee8ed5b86c9875030d358f7 🚀

--- 01-color-switcher.5c16eb3f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body;intervalId=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(intervalId),t.disabled=!1,e.disabled=!0,a.style.backgroundColor=""}))}();
-//# sourceMappingURL=01-color-switcher.5c16eb3f.js.map
