e83d17b6f371fa206c241c712a4b190149eee8de
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-10-07 09:32:55 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
+
+function startColorChange() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+};
+
+intervalId = setInterval(() => { body.style.backgroundColor = getRandomHexColor(); }, 1000);
+
+function stopColorChange() {
+    clearInterval(intervalId);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    body.style.backgroundColor = '';
+};
+
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
